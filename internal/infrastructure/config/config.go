a579af4665c807af181cfa617da86a90c3c3d976
Kamilo Cervantes <cacesa8931@gmail.com>
2024-11-12 11:27:01 -0500

    Subiendo ajustes docker

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -12,11 +12,7 @@ type Config struct {
 func LoadConfig() *Config {
 	return &Config{
 		ServerAddress: getEnv("SERVER_ADDRESS", ":8080"),
-<<<<<<< Updated upstream
-		DatabaseDSN:   getEnv("DATABASE_DSN", "host=localhost user=postgres password=camila dbname=db_url port=5432 sslmode=disable"),
-=======
 		DatabaseDSN:   getEnv("DATABASE_DSN", "host=database user=lando password=lando dbname=proyectogo port=5432 sslmode=disable"),
->>>>>>> Stashed changes
 	}
 }
 
